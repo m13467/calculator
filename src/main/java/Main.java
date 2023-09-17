13b71f219c5f1bb0011f4b6ebda10b8a2b104fbe
MacKenzie Fisher <89889979+m13467@users.noreply.github.com>
2023-09-16 20:29:05 -0600

    Finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,22 @@
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Testing Dockerfile");
+        Calculator calc = new Calculator();
+        if (Objects.equals(args[0], "add")) {
+            System.out.println(calc.add(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
+        } else if (Objects.equals(args[0], "subtract")) {
+            System.out.println(calc.subtract(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
+        } else if (Objects.equals(args[0], "multiply")) {
+            System.out.println(calc.multiply(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
+        } else if (Objects.equals(args[0], "divide")) {
+            System.out.println(calc.divide(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
+        } else if (Objects.equals(args[0], "binary")) {
+            System.out.println(calc.intToBinaryNumber(Integer.parseInt(args[1])));
+        } else if (Objects.equals(args[0], "fibonacci")) {
+            System.out.println(calc.fibonacciNumberFinder(Integer.parseInt(args[1])));
+        } else {
+            System.out.println("error");
+        }
     }
 }
